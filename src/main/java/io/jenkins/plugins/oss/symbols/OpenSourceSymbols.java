562f9c1757537906a358c86544e51cb488cb3bdb
Valentin Delaye <jonesbusy@users.noreply.github.com>
2026-01-14 13:30:16 +0100

    Migrate Commons Lang from 2 to 3

--- src/main/java/io/jenkins/plugins/oss/symbols/OpenSourceSymbols.java
+++ src/main/java/io/jenkins/plugins/oss/symbols/OpenSourceSymbols.java
@@ -11,7 +11,7 @@ import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Stream;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 public final class OpenSourceSymbols {
 
